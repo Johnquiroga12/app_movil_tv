620199caf1a7be3114a0e422f3d3a01d120c65db
Johnquiroga12 <100298882+Johnquiroga12@users.noreply.github.com>
2022-07-17 18:59:51 -0500

    ise lo q pude
    john

--- app_tv_movil/app/src/main/java/com/john/quiroga/app_tv_movil/MovieList.java
+++ app_tv_movil/app/src/main/java/com/john/quiroga/app_tv_movil/MovieList.java
@@ -5,12 +5,11 @@ import java.util.List;
 
 public final class MovieList {
     public static final String MOVIE_CATEGORY[] = {
-            "Category Zero",
-            "Category One",
-            "Category Two",
-            "Category Three",
-            "Category Four",
-            "Category Five",
+            "PELICULAS ",
+            "SERIES",
+            "ANIME",
+            "NOVELAS",
+
     };
 
     private static List<Movie> list;
@@ -26,21 +25,16 @@ public final class MovieList {
     public static List<Movie> setupMovies() {
         list = new ArrayList<>();
         String title[] = {
-                "Zeitgeist 2010_ Year in Review",
+                "RAPIDOS Y FURIOSOS",
                 "Google Demo Slam_ 20ft Search",
-                "Introducing Gmail Blue",
+                "Introducing Gmail Red",
                 "Introducing Google Fiber to the Pole",
                 "Introducing Google Nose"
         };
 
-        String description = "Fusce id nisi turpis. Praesent viverra bibendum semper. "
-                + "Donec tristique, orci sed semper lacinia, quam erat rhoncus massa, non congue tellus est "
-                + "quis tellus. Sed mollis orci venenatis quam scelerisque accumsan. Curabitur a massa sit "
-                + "amet mi accumsan mollis sed et magna. Vivamus sed aliquam risus. Nulla eget dolor in elit "
-                + "facilisis mattis. Ut aliquet luctus lacus. Phasellus nec commodo erat. Praesent tempus id "
-                + "lectus ac scelerisque. Maecenas pretium cursus lectus id volutpat.";
+        String description = "Fusce id nisi turpis. Praesent viverra bibendum semper. ";
         String studio[] = {
-                "Studio Zero", "Studio One", "Studio Two", "Studio Three", "Studio Four"
+                "PELICULAS", "SERIES", "ANIME", "NOVELAS", "NOTICIAS"
         };
         String videoUrl[] = {
                 "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/Zeitgeist/Zeitgeist%202010_%20Year%20in%20Review.mp4",
@@ -50,18 +44,25 @@ public final class MovieList {
                 "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Nose.mp4"
         };
         String bgImageUrl[] = {
-                "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/Zeitgeist/Zeitgeist%202010_%20Year%20in%20Review/bg.jpg",
-                "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/Demo%20Slam/Google%20Demo%20Slam_%2020ft%20Search/bg.jpg",
-                "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Gmail%20Blue/bg.jpg",
+                "https://rtvc-assets-radionica3.s3.amazonaws.com/s3fs-public/2022-02/animes.jpg",
+                "https://i.blogs.es/6449f7/espinof-peliculas-mas-esperadas-estrenos-cine-2018/840_560.jpg",
+                "https://internetpasoapaso.com/wp-content/uploads/%C2%BFCu%C3%A1les-son-las-mejores-p%C3%A1ginas-web-para-ver-novelas-Online-gratis-y-completas-Lista.jpg",
                 "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Fiber%20to%20the%20Pole/bg.jpg",
                 "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Nose/bg.jpg",
         };
         String cardImageUrl[] = {
-                "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/Zeitgeist/Zeitgeist%202010_%20Year%20in%20Review/card.jpg",
-                "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/Demo%20Slam/Google%20Demo%20Slam_%2020ft%20Search/card.jpg",
-                "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Gmail%20Blue/card.jpg",
-                "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Fiber%20to%20the%20Pole/card.jpg",
-                "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Nose/card.jpg"
+                //RAPIDOS
+                "https://mx.web.img3.acsta.net/pictures/21/04/14/17/28/5059871.jpg",
+//SERIE
+                "https://1.bp.blogspot.com/-2FUUi16sUv8/YY5ZBNFQCkI/AAAAAAABJIY/VJr6UzLkJWA5ItwLPACCkIPm2JCXKTTfQCLcBGAsYHQ/s1350/poster%2Bperdidos%2Ben%2Bel%2Bespacio%2Btemporada%2Bfinal.jpg",
+
+       //ANIME
+                "https://img1.ak.crunchyroll.com/i/spire3/98ea4207e23a2ea3e3af39ad641d88001533322009_main.jpg",
+//NOVELA
+                "",
+                "https://cdnb.20m.es/sites/139/2022/05/9788419301000-196x300.jpg",
+
+
         };
 
         for (int index = 0; index < title.length; ++index) {
@@ -73,10 +74,13 @@ public final class MovieList {
                             videoUrl[index],
                             cardImageUrl[index],
                             bgImageUrl[index]));
-        }
-
+            }
         return list;
     }
+
+
+
+
 
     private static Movie buildMovieInfo(
             String title,
