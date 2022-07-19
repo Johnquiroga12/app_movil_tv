7cd812be9d33aa6f7e521f548dd53c2c6457ce48
Arust-INSYS <90620456+Arust-INSYS@users.noreply.github.com>
2022-07-18 21:04:41 -0500

    cambios

--- app_tv_movil/app/src/main/java/com/john/quiroga/app_tv_movil/MovieList.java
+++ app_tv_movil/app/src/main/java/com/john/quiroga/app_tv_movil/MovieList.java
@@ -55,11 +55,11 @@ public final class MovieList {
                 "DOCUMENTAL DE HISTORIA", "DOCUMENTAL DE FLORA", "DOCUMENTAL DE FAUNA", "DOCUMENTAL DE ASTRONOMIA", "NOTICIAS"
         };
         String videoUrl[] = {
-                "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/Zeitgeist/Zeitgeist%202010_%20Year%20in%20Review.mp4",
+                "https://drive.google.com/file/d/1YpmXoGg6VP06iRORPlhReNNzwKzl_Mts/view.mp4",
                 "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/Demo%20Slam/Google%20Demo%20Slam_%2020ft%20Search.mp4",
                 "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Gmail%20Blue.mp4",
                 "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Fiber%20to%20the%20Pole.mp4",
-                "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Nose.mp4"
+                ""
         };
         String bgImageUrl[] = {
                 "https://eacnur.org/blog/wp-content/uploads/2017/09/mejores-documentales-de-historia_opt-800x400.jpg",
